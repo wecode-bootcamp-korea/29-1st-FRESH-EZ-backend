bcf9773378826a4fbabb1a2aec3a62f28a6eb520
lee <higgs95@naver.com>
2022-01-24 16:43:20 +0900

    initial setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-lso(ds5x412j$0sk5)*k=*1v(acjfa5o#_4v2ko*d-=pa34d&u'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'freshez',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
