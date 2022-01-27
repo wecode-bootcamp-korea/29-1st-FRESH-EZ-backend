b33dc41df8f6df17279d5245e8cf5672b826458d
wepungsan <wepungsan@gmail.com>
2022-01-28 07:57:20 +0900

    [EDIT] review list

--- product/views.py
+++ product/views.py
@@ -18,6 +18,8 @@ class SubscribeDetailView(View):
         product_name_list = []
         product_image_list = []
         product_price_list = []
+        product_review_count_list = []
+        product_review_score_list = []
 
         for product in product_instance_list:
             product_name_list.append(product.name)
@@ -29,14 +31,17 @@ class SubscribeDetailView(View):
 
             product_price_list.append(product.price)
 
-        review_count = random.randrange(2000, 3000)
-        review_score = round(uniform(1, 5), 1)
+            review_count = random.randrange(2000, 3000)
+            product_review_count_list.append(review_count)
+
+            review_score = round(uniform(1, 5), 1)
+            product_review_score_list.append(review_score)
 
         return JsonResponse({
             "message": "SUCCESS",
             "products" : product_name_list,
             "image_list" : product_image_list,
             "price": product_price_list,
-            "review_count" : review_count,
-            "review_score" : review_score,
+            "review_count" : product_review_count_list,
+            "review_score" : product_review_score_list,
         }, status=200)
