ee74eb81482edf14d99fc09af25fb62026ff7dd7
wepungsan <wepungsan@gmail.com>
2022-01-28 12:33:43 +0900

    product app views.py 리팩토링

--- product/views.py
+++ product/views.py
@@ -11,11 +11,6 @@ from product.models import Product
 class DetailView(View):
     def post(self, request, pk):
         product = get_object_or_404(Product, id=pk)
-        name = product.name
-        category = product.category.name
-        price = product.price
-        desc = product.description
-        allergy_list = []
 
         review_count = random.randrange(2000, 3000)
         review_score = round(uniform(1, 5), 1)
@@ -24,15 +19,14 @@ class DetailView(View):
         title_image_url = productimage[0].image_url
         str_title_image_url = str(title_image_url)
 
-        for product_allergy in product.productallergy_set.all():
-            allergy_list.append(product_allergy.allergy.name)
+        allergy_list = [product_allergy.allergy.name for product_allergy in product.productallergy_set.all()]
 
         return JsonResponse({
             "message" : "SUCCESS",
-            "name" : name,
-            "category" : category,
-            "price" : price,
-            "desc" : desc,
+            "name" : product.name,
+            "category" : product.category.name,
+            "price" : product.price,
+            "desc" : product.description,
             "allergy" : allergy_list,
             "title_image_url" : str_title_image_url,
             "review_count" : review_count,
