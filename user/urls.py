7a8bf18ffea93081b9c55b9c5417789c48819a1f
lee <higgs95@naver.com>
2022-01-30 21:06:07 +0900

    [ADD] 로그인 뷰

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from user.views import AllergyInfoView,SignUpView
+from user.views import AllergyInfoView, SignUpView, SignInView
 
 urlpatterns = [
     path('/allergy', AllergyInfoView.as_view()),
     path('/signup', SignUpView.as_view()),
+    path('/signin', SignInView.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -1,12 +1,12 @@
-import json
+import json, bcrypt, jwt
 
-import bcrypt
 from django.core.exceptions import ValidationError
 from django.http import JsonResponse
 from django.views import View
 
 from user.models import User, Allergy, UserAllergy
 from user.validator import validate_email, validate_password, validate_email_duplicate
+from my_settings import JWT_SECRET_KEY, ALGORITHM
 
 class AllergyInfoView(View):
     def get(self, request):
@@ -63,3 +63,24 @@ class SignUpView(View):
             return JsonResponse({'message' : 'KEYERROR'}, status = 400)
         except ValidationError as e:
             return JsonResponse({'message' : e.message}, status = 400)
+
+class SignInView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+
+        try:
+            email = data['email']
+            password = data['password']
+            user = User.objects.get(email=email)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
+        except User.DoesNotExist:
+            return JsonResponse({"message" : "INVALID_USER (email)"}, status=401)
+
+        if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+            return JsonResponse({"message" : "INVALID_USER (password)"}, status=401)
+
+        access_token = jwt.encode({'id': user.id},JWT_SECRET_KEY,algorithm=ALGORITHM)
+
+        return JsonResponse({"message" : "SUCCESS", "JWT" : access_token}, status=200)
